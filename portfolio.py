c70613c3c1a364dc58db01965bc3e471b0cef091
Sayed Moustafa <72506048+Alsharifs@users.noreply.github.com>
2026-01-07 08:57:14 +0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -133,7 +133,7 @@ st.write("")
 m1, m2, m3, m4 = st.columns(4)
 with m1: st.markdown('<div class="metric-container"><div class="metric-value">10+ Years</div><div>Workforce & Operational Analytics</div></div>', unsafe_allow_html=True)
 with m2: st.markdown('<div class="metric-container"><div class="metric-value">24x Faster</div><div>Automation Impact (4m to 5s)</div></div>', unsafe_allow_html=True)
-with m3: st.markdown('<div class="metric-container"><div class="metric-value">3 Languages</div><div>Python, C#, and JavaScript</div></div>', unsafe_allow_html=True)
+with m3: st.markdown('<div class="metric-container"><div class="metric-value">3 programming Languages</div><div>Python, C#, and JavaScript</div></div>', unsafe_allow_html=True)
 with m4: st.markdown('<div class="metric-container"><div class="metric-value">Big Data</div><div>SQL, Power BI & DWH Expert</div></div>', unsafe_allow_html=True)
 
 st.divider()
@@ -294,6 +294,7 @@ st.markdown('</div>', unsafe_allow_html=True)
 # --- Footer ---
 st.divider()
 st.markdown("<p style='text-align: center; color: grey;'> Developed by | Sayed Moustafa© 2026</p>", unsafe_allow_html=True)
+
 
 
 
